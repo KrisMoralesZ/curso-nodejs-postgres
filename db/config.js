8e7631fe97512d2a893abf8b87aa22d1a95eb240
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2024-07-12 14:34:57 -0700

    create migrations

--- db/config.js
+++ db/config.js
@@ -1,14 +1,16 @@
-const { Sequelize } = require('sequelize')
-
 const { config } = require('../config/config,js');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-const sequelize = new Sequelize(URI, {
-  dialect: 'postgres',
-  logging: true,
-});
-
-module.exports = sequelize;
+module.exports = {
+  development: {
+    url: URI,
+    dialect: 'postgres',
+  },
+  production: {
+    url: URI,
+    dialect: 'postgres',
+  }
+};
